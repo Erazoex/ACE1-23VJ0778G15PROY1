6a452c07e5343384625135e5522cdde987ba0206
HugoGG202004807 <3056736960301@ingenieria.usac.edu.gt>
2023-06-14 23:22:42 -0600

    Secuencia inicial

--- arduino/simulado/simulado.ino
+++ arduino/simulado/simulado.ino
@@ -2,11 +2,13 @@
 
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
+char mensaje[] = "Diego - Brian - Hugo 202004807 - Victor - Henry";
+
 byte userCounter = 0;
 struct User {
-    string name;
+    String name;
     int phoneNumber;
-    string password;    
+    String password;    
 };
 
 int logCounter = 1;
@@ -15,12 +17,40 @@ struct Log {
     char Description[15];
 };
 
-void setup() {
+void MensajeInicio() {
+  int longitudMensaje = strlen(mensaje);
   
+  for (int desplazamiento = 0; desplazamiento <= longitudMensaje + 16; desplazamiento++) {
+    lcd.clear();
+    
+    // Calcula la posición de inicio del mensaje en función del desplazamiento
+    int posicionInicio = max(0, desplazamiento - 16);
+    
+    // Calcula la posición de fin del mensaje
+    int posicionFin = min(longitudMensaje, desplazamiento);
+    
+    // Imprime el mensaje desplazado
+    lcd.setCursor(0, 0);
+    lcd.print(mensaje + posicionInicio);
+    if (posicionFin > 16) {
+      lcd.setCursor(posicionFin - 16, 1);
+      lcd.print(mensaje + posicionInicio + 16);
+    }
+    
+    delay(20);  // Pausa de 20 milisegundos
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16,4);
+  // Imprime el mensaje inicialmente en la primera fila
+  lcd.setCursor(0, 0);
+  lcd.print(mensaje);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  MensajeInicio();
 
 }
